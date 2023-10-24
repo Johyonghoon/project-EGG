110704371eefb0c2001f9758a6d59c6f9e27b603
ggobug <139421105+ggobug@users.noreply.github.com>
2023-10-24 13:34:00 +0900

    Revert "[기능 추가] Problem 모델 정의"

--- planting_grass/wsgi.py
+++ planting_grass/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for egg project.
+WSGI config for planting_grass project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'egg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planting_grass.settings')
 
 application = get_wsgi_application()
