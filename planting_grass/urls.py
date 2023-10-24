e7d66ff2c18913455e448d29884c6707c2cfb742
ggobug <139421105+ggobug@users.noreply.github.com>
2023-10-24 13:08:19 +0900

    Update urls.py

--- planting_grass/urls.py
+++ planting_grass/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for planting_grass project.
+URL configuration for egg project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
