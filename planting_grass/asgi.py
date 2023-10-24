ee939a138b1aee2ac7a28c6d89748c1aedfb22b0
ggobug <139421105+ggobug@users.noreply.github.com>
2023-10-24 13:04:39 +0900

    Update asgi.py

--- planting_grass/asgi.py
+++ planting_grass/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planting_grass.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'egg.settings')
 
 application = get_asgi_application()
