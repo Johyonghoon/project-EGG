b5c9ac548f0166dd7228a93521948a11143c3a4b
Johyonghoon <johyonghoon01@gmail.com>
2023-11-03 17:57:57 +0900

    copy practice page

--- scripts/sprint_list.js
+++ scripts/sprint_list.js
@@ -5,6 +5,17 @@ if (typeof(browser) === 'undefined') {
   console.log(page404Content)
   if ( page404Content ) {
     page404Content.innerHTML = ''
+
+    // 연습 페이지에서 데이터를 가져오는 요청
+    axios('https://www.acmicpc.net/group/practice/18558')
+    .then(response => response.text())
+    .then(data => {
+      // 데이터를 스프린트 페이지에 삽입
+      page404Content.innerHTML = data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
   }
 }
 
